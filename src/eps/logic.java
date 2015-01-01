e6e158c63c99d3d3f6d72e67f51c35103df5a01f
Dave <alexanderankin@gmail.com>
2015-01-01 18:01:44 -0500

    Moved creation of genes into own method

--- src/eps/logic.java
+++ src/eps/logic.java
@@ -11,27 +11,31 @@ public class logic {
 	public static final int NUMBEROFGENES = 3;
 	public static int POPULTIONSIZE = 0; // get from file method
 
+	public static String[] genes = new String[NUMBEROFGENES];
+
 	public static void main(String[] args) throws IOException {
 		readCSV();
-
+		createGenes();
 	}
 
 	public static void readCSV() throws IOException {
 		Scanner s;
 		s = new Scanner(new File("src/bsData.csv"));
-		String[] genes = new String[NUMBEROFGENES];
 
 		for (int i = 0; s.hasNext(); i++) {
 			genes[i] = s.nextLine();
 		}
 		POPULTIONSIZE = (genes[0].length() / 3) + 1;
+
+		s.close();
+	}
+
+	public static void createGenes() {
 		List<CatGene> catgenes = new ArrayList<CatGene>();
 		int j = 0;
 		while (j < NUMBEROFGENES) {
 			catgenes.add(new CatGene(j, genes[j]));
 			j++;
 		}
-		s.close();
 	}
-
 }
