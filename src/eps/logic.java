c7bc9da19835e3e2260c0988a69df2728d8b7df2
Dave <alexanderankin@gmail.com>
2015-01-02 01:41:16 -0500

    Version 1.0 release, ready for data.

--- src/eps/logic.java
+++ src/eps/logic.java
@@ -10,12 +10,21 @@ import java.util.List;
 public class logic {
 	public static final int NUMBEROFGENES = 3; // must change in THIS file
 	public static int POPULTIONSIZE = 0; // get from file method
-
+	
 	public static String[] genes = new String[NUMBEROFGENES];
+	
+	public static List<CatGene> catgenes = new ArrayList<CatGene>();
+	public static double allGeneF = 0.0;
+	
+	public static int EffectivePopulation = 0;
 
 	public static void main(String[] args) throws IOException {
 		readCSV();
 		createGenes();
+		findF();
+		EffectivePopulation = (int) (allGeneF * POPULTIONSIZE);
+		System.out.println("Effective Population is: " + EffectivePopulation);
+		
 	}
 
 	public static void readCSV() throws IOException {
@@ -30,11 +39,19 @@ public class logic {
 	}
 
 	public static void createGenes() {
-		List<CatGene> catgenes = new ArrayList<CatGene>();
+		
 		int j = 0;
 		while (j < NUMBEROFGENES) {
 			catgenes.add(new CatGene(j, genes[j]));
 			j++;
 		}
+	}
+	
+	public static void findF() { //D ALGORITHM
+		
+		for (CatGene a : catgenes) {
+			allGeneF += a.bigF * 1.0 / catgenes.size();
+		}
+		
 	}
 }
